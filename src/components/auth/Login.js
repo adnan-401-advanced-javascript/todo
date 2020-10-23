33c0b8beccfc64ef9d26b7c035f774f29ff52cd3
amuammer <amuammer@students.iugaza.edu.ps>
2020-10-23 16:52:01 +0300

    adding auth

---
+++ src/components/auth/Login.js
@@ -0,0 +1,46 @@
+import React, { useContext, useState } from 'react';
+import { AuthContext } from './context.js';
+
+const Login = () => {
+
+    const context = useContext(AuthContext);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        context.login(username, password);
+    };
+
+    return (
+        <>
+            <form inline
+            onSubmit={handleSubmit}
+            className=" mr-sm-10"
+            >
+
+
+              <input
+                className="mr-sm-5"
+                placeholder='UserName'
+                name='username'
+                type='text'
+                onChange={(e) => setUsername(e.target.value)}
+                />
+
+                <input
+                className="mr-sm-5"
+                placeholder='password'
+                name='password'
+                type='text'
+                onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button type="submit" variant="dark" className="mr-sm-4" size="md">Login</button>
+                <button onClick={() => context.setSwitchToSignup(true)}>Signup</button>
+            </form>
+        </>
+    );
+};
+
+export default Login;
--- src/hooks/useAjax.js
+++ src/hooks/useAjax.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 export default () => {
 
-    const axiosApiInstance = (url, method, body) => {
+    const axiosApiInstance = (url, method, body, moreHeaders) => {
       return axios({
         url: url,
         method: method,
         mode: 'cors',
         cache: 'no-cache',
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json', ...moreHeaders },
         data: body
       })
     }
